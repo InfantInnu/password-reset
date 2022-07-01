4996ea37ef973a253a0db09b7fb766556e2e64ff
infant <y.infant1996@gmail.com>
2022-07-01 18:47:40 +0530

    password-reset

---
+++ src/Msg.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Msg() {
+  return (
+    <div className="m-5 text-center ">Reset password link was sent to your email. Please check your mail !</div>
+  )
+}
+
+export default Msg
